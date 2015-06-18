02c2c3e41afaed700f1bea2111c36e320439d34c
pviver <pviver@25dc53a2-33d8-4e01-8b70-898bf8f7cf2b>
2015-06-18 13:27:03 +0000

    Ajout de la classe FDANTS_Identite

---
+++ lib/FDLib/FDANTS_Identite.class.php
@@ -0,0 +1,45 @@
+<?php
+//https://datafranceconnect.opendatasoft.com/api/records/1.0/search?dataset=ants_identite
+
+class FDANTSIdentite extends FournisseurDonnees {
+
+	public function getId(){
+		return "fd_ants_identite";
+	}
+
+	public function getName(){
+		return "ANTS Identité";
+	}
+
+	public function getScope(){
+		return "ants_identite";
+	}
+
+	public function getProvidedInfo(){
+		return array(
+				"numero_passeport"=>"Numéro de passeport",
+				"hypothese_documents_d_identite"=>"Type de documents d'identité",
+				"sexe"=>"Sexe",
+				"numero_cni"=>"Numéro de la Carte Nationnale d'Identité",
+				"date_de_naissance"=>"Date de naissance",
+				"date_delivrance_cni"=>"Date de délivrance de la CNI",
+				"date_delivrance_passeport"=>"Date de délivrance du passeport",
+				"couleur_des_yeux"=>"Couleur des yeux",
+				"nom_de_naissance"=>"Nom de famille",
+				"taille"=>"Taille",
+				"lieu_de_naissance"=>"Code postale de la ville de naissance",
+				"prenoms"=>"Prénom",
+				"pays_de_naissance"=>"Pays de naissance"
+					);
+	}
+
+	protected function getFDURL(){
+		return "https://datafranceconnect.opendatasoft.com/api/records/1.0/search?dataset=ants_identite";
+	}
+
+	public function getInfo($access_token){
+		$info = parent::getInfo($access_token);
+		return $info['records'][0]['fields'];
+	}
+
+}
