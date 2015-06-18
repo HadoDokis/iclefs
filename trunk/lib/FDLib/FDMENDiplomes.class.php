758e47a557af8cadf12055ad2730c9b8512572e8
pviver <pviver@25dc53a2-33d8-4e01-8b70-898bf8f7cf2b>
2015-06-18 14:38:12 +0000

    Ajout de la classe FDMENDiplomes

---
+++ trunk/lib/FDLib/FDMENDiplomes.class.php
@@ -0,0 +1,41 @@
+<?php
+//https://datafranceconnect.opendatasoft.com/api/records/1.0/search?dataset=men_diplomes
+class FDMENDiplomes extends FournisseurDonnees {
+
+	public function getId(){
+		return "men_dimples";
+	}
+
+	public function getName(){
+		return "Diplome d'Etat";
+	}
+
+	public function getScope(){
+		return "men_diplome";
+	}
+
+	public function getProvidedInfo(){
+		return array(
+				        "sexe":"Sexe",
+    "mention"=>"Mention",Seri
+    "date_de_naissance"=>"Date de naissance",
+    "serie"=>"Filière",
+    "academie_d_origine"=>"Identifiant d'Académie",
+    "session"=>"Session",
+    "nom_de_naissance"=>"Nom de famille",
+    "examen"=>"Examen",
+    "prenoms"=>"Prénom(s)",
+    "pays_de_naissance"=>"Pays de naissance"
+				);
+	}
+
+	protected function getFDURL(){
+		return "https://datafranceconnect.opendatasoft.com/api/records/1.0/search?dataset=men_diplomes";
+	}
+
+	public function getInfo($access_token){
+		$info = parent::getInfo($access_token);
+		return $info['records'][0]['fields'];
+	}
+
+}
