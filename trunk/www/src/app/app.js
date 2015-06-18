78927e260ef253e4cb1879dcfb7cca24616ef8b1
lhameury <lhameury@25dc53a2-33d8-4e01-8b70-898bf8f7cf2b>
2015-06-18 16:51:47 +0000

    Ajout de la partie frontend

---
+++ trunk/www/src/app/app.js
@@ -0,0 +1,25 @@
+angular.module( 'iClefs', [
+  'templates-app',
+  'templates-common',
+  'iClefs.form',
+  'iClefs.about',
+  'ui.router'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/form' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | iClefs';
+    }
+  });
+})
+
+;
+
